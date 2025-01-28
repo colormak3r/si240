d6a06a379af4042942f2785dd23114b66885e186
Khoa Nguyen <colormak3r@gmail.com>
2025-01-27 22:48:01 -0800

    Added comments to Hello World Program

--- hello_world_incomplete/hello.asm
+++ hello_world_incomplete/hello.asm
@@ -1,3 +1,71 @@
+; Program name: "Program Name". A short description of the purpose of the program
+; Copyright (C) <2023>  <Your Name>
+
+; This file is part of the software program "Program Name".
+
+; "Program Name" is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+
+; "Program Name" is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+; Author information
+;   Author name : Code Goblin
+;   Author email: instalock_caitlyn@bronze2.botlane
+;   Author section: 240-99
+;   Author CWID : 000000000
+
+; For research purpose only. Please don't copy word for word. Avoid academic dishonesty.
+
 segment .data
 segment.bss
 segment .text
+hello_world:
+    ; Save the base pointer
+    push    rbp
+    mov     rbp, rsp
+
+    ; Save the general purpose registers
+    push    rbx
+    push    rcx
+    push    rdx
+    push    rsi
+    push    rdi
+    push    r8 
+    push    r9 
+    push    r10
+    push    r11
+    push    r12
+    push    r13
+    push    r14
+    push    r15
+    pushf
+
+    ; Restore the general purpose registers
+    popf          
+    pop     r15
+    pop     r14
+    pop     r13
+    pop     r12
+    pop     r11
+    pop     r10
+    pop     r9 
+    pop     r8 
+    pop     rdi
+    pop     rsi
+    pop     rdx
+    pop     rcx
+    pop     rbx
+
+    ; Restore the base pointer
+    pop     rbp
+
+    ; Return
+    ret
